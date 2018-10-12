86b7b643d74cb8c16949663a3cf2bfa82f8540d9
Juan Santiago Martín Balbás <jmbalbas@gmail.com>
2018-10-12 11:03:43 +0200

    Initial Commit

---
+++ PokemonApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonApp
+//
+//  Created by Juan Santiago Martín Balbás on 12/10/2018.
+//  Copyright © 2018 Juan Santiago Martín Balbás. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
