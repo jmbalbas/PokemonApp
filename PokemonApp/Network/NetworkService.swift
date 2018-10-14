ae1c8248ba2617f8b42d221a2a4c635dd89cf9be
Juan Santiago Martín Balbás <jmbalbas@gmail.com>
2018-10-14 13:44:13 +0200

    Added cache for requests

--- PokemonApp/Network/NetworkService.swift
+++ PokemonApp/Network/NetworkService.swift
@@ -12,6 +12,8 @@ import Alamofire
 final class NetworkService {
     static var baseUrl = "https://pokeapi.co/api/v2"
     
+    private static let requestCache = NSCache<AnyObject, AnyObject>()
+    
     enum EndPoint {
         case getPokemon//, getSpecie
         
@@ -26,7 +28,14 @@ final class NetworkService {
     }
     
     static func getPokemons(completionHandler: @escaping (PokemonListResponseModel?, Error?) -> ()) {
-        Alamofire.request(baseUrl + EndPoint.getPokemon.description, method: .get, parameters: nil).validate().responseJSON { (response) in
+        let url = baseUrl + EndPoint.getPokemon.description
+        
+        if let cachedResponseModel = getCachedRequest(urlString: url, responseModelType: PokemonListResponseModel.self) {
+            completionHandler(cachedResponseModel, nil)
+            return
+        }
+        
+        Alamofire.request(url, method: .get, parameters: nil).validate().responseJSON { (response) in
             var result: PokemonListResponseModel?
             let error = response.error
             
@@ -35,6 +44,8 @@ final class NetworkService {
             guard response.result.isSuccess else { return }
 
             if let data = response.data, let pokemonListResponseModel = try? JSONDecoder().decode(PokemonListResponseModel.self, from: data) {
+                requestCache.setObject(pokemonListResponseModel as AnyObject, forKey: url as AnyObject)
+
                 result = pokemonListResponseModel
             }
         }
@@ -42,6 +53,12 @@ final class NetworkService {
     
     static func getPokemon(withName name: String, completionHandler: @escaping (PokemonResponseModel?, Error?) -> ()) {
         let url = baseUrl + EndPoint.getPokemon.description + name
+        
+        if let cachedResponseModel = getCachedRequest(urlString: url, responseModelType: PokemonResponseModel.self) {
+            completionHandler(cachedResponseModel, nil)
+            return
+        }
+        
         Alamofire.request(url, method: .get, parameters: nil).validate().responseJSON { (response) in
             var result: PokemonResponseModel?
             let error = response.error
@@ -50,13 +67,19 @@ final class NetworkService {
             
             guard response.result.isSuccess else { return }
             
-            if let data = response.data, let pokemonListResponseModel = try? JSONDecoder().decode(PokemonResponseModel.self, from: data) {
-                result = pokemonListResponseModel
+            if let data = response.data, let pokemonResponseModel = try? JSONDecoder().decode(PokemonResponseModel.self, from: data) {
+                requestCache.setObject(pokemonResponseModel as AnyObject, forKey: url as AnyObject)
+                result = pokemonResponseModel
             }
         }
     }
         
     static func getSpecie(fromURL url: URL, completionHandler: @escaping (PokemonSpecieResponseModel?, Error?) -> ()) {
+        if let cachedResponseModel = getCachedRequest(urlString: url.absoluteString, responseModelType: PokemonSpecieResponseModel.self) {
+            completionHandler(cachedResponseModel, nil)
+            return
+        }
+        
         Alamofire.request(url, method: .get, parameters: nil).validate().responseJSON { (response) in
             var result: PokemonSpecieResponseModel?
             let error = response.error
@@ -65,10 +88,19 @@ final class NetworkService {
             
             guard response.result.isSuccess else { return }
             
-            if let data = response.data, let pokemonListResponseModel = try? JSONDecoder().decode(PokemonSpecieResponseModel.self, from: data) {
-                result = pokemonListResponseModel
+            if let data = response.data, let pokemonSpecieResponseModel = try? JSONDecoder().decode(PokemonSpecieResponseModel.self, from: data) {
+                requestCache.setObject(pokemonSpecieResponseModel as AnyObject, forKey: url.absoluteString as AnyObject)
+                result = pokemonSpecieResponseModel
             }
         }
+    }
+    
+    private static func getCachedRequest<T>(urlString: String, responseModelType: T.Type) -> T? where T : Decodable {
+        if let responseModel = requestCache.object(forKey: urlString as AnyObject) as? T {
+            return responseModel
+        }
+        
+        return nil
     }
     
 }
